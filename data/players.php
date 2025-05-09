da5a203faf601730d13b9b686f0c4a13b1b684b9
Edivaldo Reis <efrj@hotmail.com>
2025-05-09 00:00:55 -0300

    Added data/player.php script and txt files for storage

---
+++ data/players.php
@@ -0,0 +1,15 @@
+<?php
+
+$playerNames = [
+    'Ana Silva', 'Bruno Oliveira', 'Carlos Santos', 'Débora Lima', 
+    'Eduardo Pereira', 'Fátima Costa', 'Gabriel Martins', 'Helena Cruz',
+    'Igor Almeida', 'Juliana Souza', 'Kleber Ramos', 'Larissa Gomes',
+    'Marcelo Dias', 'Natália Ferreira', 'Otávio Ribeiro', 'Paula Barbosa',
+    'Quentin Tarantino', 'Roberta Carvalho', 'Sérgio Mendes', 'Talita Rocha'
+];
+
+$avatarIcons = [
+    'user', 'user-tie', 'user-graduate', 'user-ninja', 'user-astronaut',
+    'user-secret', 'user-md', 'user-nurse', 'user-shield', 'user-alt'
+];
+?>
